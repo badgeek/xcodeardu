702e784225d459c75910a920f6cfb4732e6ae713
Budi Prakosa a.k.a Iyok <iyok@deadmediafm.org>
2013-02-02 15:22:59 +0700

    8000Hz, fixed timer compare register

--- 8bit-mixed-tape/firmware/main.c
+++ 8bit-mixed-tape/firmware/main.c
@@ -24,7 +24,6 @@ void timer_init()
 {
     //PWM SOUND OUTPUT
     TCCR0A |= (1<<WGM00)|(1<<WGM01); //Fast pwm
-    
     //TCCR0A |= (1<<WGM00) ; //Phase correct pwm
     
     TCCR0A |= (1<<COM0A1); //Clear OC0A/OC0B on Compare Match when up-counting.
@@ -35,15 +34,15 @@ void timer_init()
     //babygnusb attiny85 clock frequency = 16.5 Mhz
     TCCR1 |= _BV(CTC1); //clear timer on compare
     //TCCR1 |= _BV(CS10); // prescale 1
-    TCCR1 |= _BV(CS11); // prescale 2
-    //TCCR1 |= _BV(CS10)|_BV(CS12); // prescale 16    
+    //TCCR1 |= _BV(CS11); // prescale 2
+    TCCR1 |= _BV(CS10)|_BV(CS12); // prescale 16    
     //TCCR1 |= _BV(CS11)|_BV(CS12); // prescale 32
     //TCCR1 |= _BV(CS10)|_BV(CS11)|_BV(CS12); // prescale 64
     //TCCR1 |= _BV(CS13); // prescale 128
     //TCCR1 |= _BV(CS10) | _BV(CS13); // prescale 256
     TIMSK |= _BV(OCIE1A); //activate compare interruppt
     //OCR1A = 3; // (16500000/2)/387 = 22050Hz
-    OCR1C = 100; // (16500000/2)/187 = 44100Hz
+    OCR1C = 125; // (16500000/2)/187 = 44100Hz
     TCNT1 = 0; //init count
     
     sei(); //enable global interrupt
