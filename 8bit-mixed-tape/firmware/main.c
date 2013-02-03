3295bbfcf5b0a45241140466042797127241ed27
Budi Prakosa a.k.a Iyok <iyok@deadmediafm.org>
2013-02-03 16:04:27 +0700

    nice fx

--- 8bit-mixed-tape/firmware/main.c
+++ 8bit-mixed-tape/firmware/main.c
@@ -27,9 +27,8 @@ volatile int j = 50;
 
 
 //ADMUX ADC
-volatile uint8_t adc1 = _BV(ADLAR) | _BV(MUX1); //PB4-ADC2
-volatile uint8_t adc2 = _BV(ADLAR) | _BV(MUX0) | _BV(MUX1); //PB3-ADC3
-
+volatile uint8_t adc2 = _BV(ADLAR) | _BV(MUX1); //PB4-ADC2 pot1
+volatile uint8_t adc3 = _BV(ADLAR) | _BV(MUX0) | _BV(MUX1); //PB3-ADC3 pot2
 
 
 void adc_init()
@@ -38,8 +37,7 @@ void adc_init()
     ADCSRA |= _BV(ADEN); //adc enable
     ADCSRA |= _BV(ADATE); //auto trigger
     ADCSRA |= _BV(ADPS0) | _BV(ADPS1) | _BV(ADPS2); //prescale 128
-    ADMUX  |= _BV(ADLAR); //ADLAR = 1 left adjusted
-    ADMUX  |= _BV(MUX1); //PB2
+    ADMUX  = adc3;
     ADCSRB = 0;
 }
 
@@ -74,7 +72,7 @@ void timer_init()
     //TCCR1 |= _BV(CS10) | _BV(CS13); // prescale 256    
     
     //SAMPLE RATE
-    OCR1C = 129; // (16500000/16)/8000 = 128
+    OCR1C = 128; // (16500000/16)/8000 = 128
     //OCR1C = 93; // (16500000/16)/11025 = 93
     //OCR1C = 46; // (16500000/16)/22050 = 46
     //OCR1C = 23; // (16500000/16)/44100 = 23
@@ -106,15 +104,22 @@ ISR(TIMER1_COMPA_vect)
  
     int value50 = 30;
 
-    snd = (t*5&t>>7)|(t*pot1&t>>10)  ;
+    
     
     //snd = (t * (t>>5|t>>8))>>(t>>16); //viznut
-    //snd = (t * ((t>>12|t>>pot1)&63&t>>4));//floor((delay_efek[t%99]);
+    snd = (t * ((t>>pot2|t>>8)&pot1&t>>4));//floor((delay_efek[t%99]);
     //sfx = (snd >> 4 << 7);
     //sfx = (snd >> 2 << 3);
     //sfx = sfx | j;
+    //snd = (t*5&t>>7)|(t*4&t>>10)  ;
+    //snd =  t * ((t>>12|t>>8)&63&t>>4);
+    //snd = (t * ((t>>pot2|t>>pot1)&63&t>>4));//floor((delay_efek[t%99]);
+    //sfx = (snd >> pot2 << pot1);
+    //sfx = (snd >> 2 << 3);
+    //sfx = sfx | j;
     OCR0A = snd;
-
+    //OCR1C = pot1;
+    
     j = j - 1;
     if (j <= 0) {
         j = value50;
@@ -126,6 +131,8 @@ ISR(TIMER1_COMPA_vect)
 
 ISR(ADC_vect)
 {
+    //http://joehalpin.wordpress.com/2011/06/19/multi-channel-adc-with-an-attiny85/
+    
     static uint8_t firstTime = 1;
     static uint8_t val;
     
@@ -133,13 +140,13 @@ ISR(ADC_vect)
     
     if (firstTime == 1)
         firstTime = 0;
-    else if (ADMUX  == adc1) {
-        pot1 = val;
+    else if (ADMUX  == adc3) {
+        pot2 = val;
         ADMUX = adc2;
     }
     else if ( ADMUX == adc2) {
-        pot2  = val;
-        ADMUX = adc1;
+        pot1  = val;
+        ADMUX = adc3;
     }
     
 }
